e53141e0d0e93a0fc0222b79923caba6424a57d8
Mathilde Arconte <signup@marconte.fr>
2020-04-01 17:07:06 +0200

    TP3 clean

--- api/server.js
+++ api/server.js
@@ -1,13 +1,7 @@
 'use strict';
 
-const cors = require('cors');
 const bodyParser =  require("body-parser");
 const express = require('express');
-const redis = require('redis');
-const client = redis.createClient({
-  host : process.env.REDIS_HOST,
-  port : process.env.REDIS_PORT
-})
 
 // Constants
 const PORT = '3000';
@@ -17,21 +11,6 @@ const HOST = '0.0.0.0';
 const app = express();
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }));
-
-const corsOptions = {
-  origin: [
-      "http://localhost:8090",
-  ]
-}
-app.use(cors(corsOptions));
-app.get('/', (req, res) => {
-  client.get('count', function(e,reply){
-     var count = 0;
-     if (reply) count = parseInt(reply)+1
-     res.status(200).json({ count })
-      client.set('count', count)
-  })
-});
 
 app.listen(PORT);
 console.log(`Running on http://${HOST}:${PORT}`);
